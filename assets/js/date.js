079c8bec0871a65f7933480c54c4c02847c03e4c
Edwin Chilatra <efchilatra@gmail.com>
2023-06-21 11:20:11 -0500

    Fechas

---
+++ assets/js/date.js
@@ -0,0 +1,95 @@
+/**
+ * Fechas parte 2
+ */
+
+let today = new Date();
+console.log(today);
+
+today.setHours(0, 0, 0);
+console.log(today);
+
+let aDay = new Date(2013, 1, 31);
+console.log(aDay);
+
+aDay.setDate(aDay.getDate() + 2);
+console.log(aDay);
+
+aDay.setSeconds(aDay.getSeconds() + 70);
+console.log(aDay);
+
+aDay.setDate(0);
+console.log(aDay);
+
+/**
+ * Crear una funcion flecha que calcule la cantidad de dias que faltan para que se termine el mes dado como parametro una fecha.
+ */
+
+let getDaysToEndMonth = (date) => {
+    let end = new Date(date);
+    end.setMonth(end.getMonth() + 1);
+    end.setDate(0);
+    return end.getDate() - date.getDate();
+
+};
+
+console.log('Faltan ' + getDaysToEndMonth(new Date()) + ' Dias para que se termine el mes')
+
+// Convertir fecha a números
+
+let date = Date.now();
+
+for (let i = 0; i < 100000000; i++) {
+    let algo = i * i * i;
+}
+
+let end = new Date();
+
+console.log(`EL tiempo Transcurrido es de ${end - date}ms`); // Mlisegundos
+
+/**
+ * Crear un funcion flecha llamada getDateAgo que reciba una fecha y una cantidad de dias y devuelva el dia del mes que corresponda, contando la cantidad de dias respecto a la fecha pasada.
+ * 
+ * Ejemplo: hoy es 21, si ingresamos la fecha actual junto con 1, el resultado debe ser 20
+ * Ejemplo: hoy es 21, si ingresamos la fecha actual junto con 5, el resultado debe ser 16
+ */
+
+const getDateAgo = (fecha, dias) => {
+    const nuevaFecha = new Date(fecha);
+    nuevaFecha.setDate(nuevaFecha.getDate() - dias);
+    return nuevaFecha.getDate();
+  }
+  
+  const fechaDeEjemplo = new Date();
+  console.log(getDateAgo(fechaDeEjemplo, 1)); 
+  console.log(getDateAgo(fechaDeEjemplo, 5)); 
+
+/**
+ * Crear una funcion que indique la cantidad de segundos transcurridos el dia de hoy
+ */
+
+function segundosDelDia() {
+    let ahora = new Date();
+    let hora = ahora.getHours();
+    let minutos = ahora.getMinutes();
+    let segundos = ahora.getSeconds();
+    let segundosTotales = hora * 3600 + minutos * 60 + segundos;
+    return `Han transcurrido ${segundosTotales} segundos en el día de hoy.`;
+  }
+  
+  console.log(segundosDelDia());
+
+/**
+ *  Crear una funcion que indique la cantidad de segundos que faltan para mañana 
+*/
+
+function segundosHastaManana() {
+    let ahora = new Date();
+    let hora = ahora.getHours();
+    let minutos = ahora.getMinutes();
+    let segundos = ahora.getSeconds();
+    let segundosTotales = hora * 3600 + minutos * 60 + segundos;
+    let segundosHastaMedianoche = 24 * 3600 - segundosTotales;
+    return `Faltan ${segundosHastaMedianoche} segundos para que sea mañana.`;
+  }
+  
+  console.log(segundosHastaManana());
--- assets/js/map.js
+++ assets/js/map.js
@@ -27,7 +27,7 @@ let arr = ['Juan', 'Alvarado'];
 let nombre = arr[0];
 let apellido = arr[1];
 console.log(nombre + ' ' + apellido);
-*/
+
 
 let [nombre, apellido] =arr;
 console.log(nombre);
@@ -44,6 +44,69 @@ console.log(empresa);
 
 let user = {};
 
-[user.name, user.surname, user.job, user.company];
+[user.name, user.surname, user.job, user.company] = arr;
 
 console.log(user);
+
+let [name1, name2, ... rest] = ['Mateo', 'Marcos', 'Lucas', 'Juan' ] ;
+console.log(name1);
+console.log(name2);
+
+// ¿y el resto?
+console.log(rest.length);
+
+let [name3 = 'Gest', lastname = 'Anonymous'] = ['Maria'];
+console.log(name3);
+console.log(lastname);
+
+// Desestructuracion de Objetos
+
+let options = {
+    title: 'Menu',
+    width: 300,
+    height: 500,
+};
+
+/*
+let {height: h, width: w, title} = options;
+console.log(title);
+console.log(w);
+console.log(h);
+console.log(options);
+
+let opt = {
+    title2: 'Menu',
+};
+
+let {height = 100, width = 200, title2} = opt;
+console.log(width);
+console.log(height);
+console.log(title2);
+
+let {title} = options;
+console.log(title);
+*/
+let options = {
+    title: 'Menu',
+    width: 300,
+    height: 500,
+};
+let {title, ...rest} = options;
+
+console.log(rest.width);
+
+/**
+ * TODO:
+ * Cree un objeto llamado user con las propiedades name, years, y van a obtener los valores en variables separadas incluyeno la variable isAdmin, que si no se encuentra en el objeto el valor por defecto es false.
+ */
+
+let user = {
+    name: "Carlos",
+    years: 34,
+  };
+  
+let {name, years, isAdmin = false } = user;
+  
+console.log(name);
+console.log(years);
+console.log(isAdmin);
